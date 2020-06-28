8eae53fc6bd82a94c012f31784b842923324d810
fbmoose48 <46300818+fbmoose48@users.noreply.github.com>
2020-06-28 05:00:24 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,20 @@
 #XMRSTAK=xmr-stak-rx-1.0.5
 
 # Download latest xmr-stak-rx
-wget https://github.com/fbmoose48/xmr-stak/archive/xmr-stak-rx-1.0.5.tar.gz
+#wget https://github.com/fbmoose48/xmr-stak/archive/xmr-stak-rx-1.0.5.tar.gz
+git https://github.com/fbmoose48/xmr-stak.git-b xmr-stak-rx
 
 # Unpack TAR
-tar -xvzf ~/xmr-stak-rx-1.0.5.tar.gz
+tar -xvzf ~/xmr-stak-rx.tar.gz
 
 # Move to /opt
-sudo mv ~/xmr-stak-xmr-stak-rx-1.0.5 /opt/xmr-stak-1.0.5
+sudo mv ~/xmr-stak-rx /opt/xmr-stak-rx
 
 #Create build dir
-sudo mkdir /opt/xmr-stak-1.0.5/build
+sudo mkdir /opt/xmr-stak-rx/build
 
 #Enter build dir
-cd /opt/xmr-stak-1.0.5/build
+cd /opt/xmr-stak-rx/build
 
 #Create makefile
 sudo cmake ..
@@ -25,14 +26,44 @@ sudo cmake ..
 #Install
 sudo make install
 
+# Download service files
+git https://github.com/fbmoose48/xmr-stak-systemd-service.git -b xmr-stak-service
+
+# Unpack TAR
+tar -xvzf ~/xmr-stak-service.tar.gz
+
+# Create "./start-service.sh" in /opt/xmr-stak-rx/build/bin or other appropriate directory
+
+sudo cp ./xmr-stak-service/start-service.sh /opt/xmr-stak-rx/build/bin
+
+# Create "/etc/systemd/system/xmr-stak.service"
+
+sudo cp ./xmr-stak-service/xmr-stak.service /etc/systemd/system/
+
+# Provide necessary permissions to "start-service.sh"
+
+sudo chmod 755 /opt/xmr-stak-rx/build/bin/start-service.sh
+
+# Make "start-service.sh" executable
+
+sudo chmod +x /opt/xmr-stak-rx/build/bin/start-service.sh
+
+# Enable service
+
+sudo systemctl enable xmr-stak.service
+
+# Start service
+
+sudo systemctl start xmr-stak.service
+
 # Copy relevant files from previous release
 #sudo cp /opt/xmr-stak/build/bin/amd.txt /opt/xmr-stak-1.0.5/build/bin/
-sudo cp /opt/xmr-stak/build/bin/config.txt /opt/xmr-stak-1.0.5/build/bin/
-sudo cp /opt/xmr-stak/build/bin/cpu.txt /opt/xmr-stak-1.0.5/build/bin/
-sudo cp /opt/xmr-stak/build/bin/pools.txt /opt/xmr-stak-1.0.5/build/bin/
-sudo cp /opt/xmr-stak/build/bin/start.sh /opt/xmr-stak-1.0.5/build/bin/
-sudo cp /opt/xmr-stak/build/bin/start-service.sh /opt/xmr-stak-1.0.5/build/bin/
+#sudo cp /opt/xmr-stak/build/bin/config.txt /opt/xmr-stak-1.0.5/build/bin/
+#sudo cp /opt/xmr-stak/build/bin/cpu.txt /opt/xmr-stak-1.0.5/build/bin/
+#sudo cp /opt/xmr-stak/build/bin/pools.txt /opt/xmr-stak-1.0.5/build/bin/
+#sudo cp /opt/xmr-stak/build/bin/start.sh /opt/xmr-stak-1.0.5/build/bin/
+#sudo cp /opt/xmr-stak/build/bin/start-service.sh /opt/xmr-stak-1.0.5/build/bin/
 
 # Run
-cd /opt/xmr-stak-1.0.5/build/bin
+#cd /opt/xmr-stak-1.0.5/build/bin
 #sudo ./start.sh
